8ecdd3f7281c1f09cf9c0d72f949f511611853d7
小学四年级小方 <321956414@qq.com>
2024-03-13 13:41:30 +0800

    完成部署313

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import Home from "@/views/Home";
 
 const routes = [
@@ -6,7 +6,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    // history: createWebHistory(),
+    history:createWebHashHistory(),
     routes
 })
 
